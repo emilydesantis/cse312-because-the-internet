fa91c655532f4332856f04188a79c8aa6bf8aa4a
Marvin <marvinpierrepierrre03@gmail.com>
2023-04-14 19:50:26 -0400

    flask

--- database.py
+++ database.py
@@ -1,36 +1,36 @@
-from pymongo import MongoClient
+# #from pymongo import MongoClient
 
-mongo_client = MongoClient("mongo")
-db = mongo_client["cse312-because-the-internet"]
+# #mongo_client = MongoClient("mongo")
+# db = mongo_client["cse312-because-the-internet"]
 
-users_id_collection = db["users_id"]
-users_account = db["user_accounts"]
+# users_id_collection = db["users_id"]
+# users_account = db["user_accounts"]
 
-def get_next_id():
-    id_object = users_id_collection.find_one({})
-    if id_object:
-        next_id = int(id_object['last_id']) + 1
-        users_id_collection.update_one({}, {'$set': {'last_id': next_id}})
-        return next_id
-    else:
-        users_id_collection.insert_one({'last_id': 1})
-        return 1
+# def get_next_id():
+#     id_object = users_id_collection.find_one({})
+#     if id_object:
+#         next_id = int(id_object['last_id']) + 1
+#         users_id_collection.update_one({}, {'$set': {'last_id': next_id}})
+#         return next_id
+#     else:
+#         users_id_collection.insert_one({'last_id': 1})
+#         return 1
     
     
-def create_record(dictionary):
-    id = get_next_id()
-    #create a record of a user's username and password
-    #handle duplicate usernames
-        #check if ursenamgit add
-    users_account.insert_one({"id": int(id), "username": dictionary.get("username"), "password": dictionary.get("password")})
-    dic = {"id": int(id), "username": dictionary.get("username"), "password": dictionary.get("password")}
-    return dic
+# def create_record(dictionary):
+#     id = get_next_id()
+#     #create a record of a user's username and password
+#     #handle duplicate usernames
+#         #check if ursenamgit add
+#     users_account.insert_one({"id": int(id), "username": dictionary.get("username"), "password": dictionary.get("password")})
+#     dic = {"id": int(id), "username": dictionary.get("username"), "password": dictionary.get("password")}
+#     return dic
 
-#how do we handle  duplicate usernames?
-def update_username(id, dictionary):
-    #update username
-    users_account.update_one({"id":int(id)},{"$set":{"username":dictionary.get("username")}})
-    dic = {{"username":dictionary.get("username")}}
-    return dic
+# #how do we handle  duplicate usernames?
+# def update_username(id, dictionary):
+#     #update username
+#     users_account.update_one({"id":int(id)},{"$set":{"username":dictionary.get("username")}})
+#     dic = {{"username":dictionary.get("username")}}
+#     return dic
 
 
--- server.py
+++ server.py
@@ -1,70 +1,27 @@
-import socketserver 
+import socketserver
 import database
 import json
 import hashlib
 import base64
-#from flask import Flask
-#from flask_sock import Sock
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
+@app.route('/')
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+def html():
+   return render_template('frontend/index.html')
 
-    def handle(self):
-        received_data = self.request.recv(2048)
-        header = received_data[ :received_data.find(b'\r\n\r\n')]
-        request = header.split(b'\r\n')
+def css():
+   return render_template('frontend/style.css')
 
-        if b'GET / ' in request[0]:
-            with open("frontend/index.html","rb") as html_file: 
-                bytearray = html_file.read()
-                file_length = len(bytearray)
-                self.request.sendall((f"HTTP/1.1 200 OK\r\nContent-Length: "+str(file_length)+"\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/html; charset=utf-8\r\n\r\n").encode()+bytearray)
-                
-        elif b'/loading.js' in request[0]:
-            with open("frontend/loading.js", "rb") as js_file:
-                bytearray = js_file.read()
-                file_length = len(bytearray)
-                self.request.sendall((f"HTTP/1.1 200 OK\r\nContent-Length:"+str(file_length)+"\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/javascript; charset=utf-8\r\n\r\n").encode()+bytearray)
-        
-        elif b'/main.js' in request[0]:
-            with open("frontend/main.js", "rb") as css_file:
-                bytearray = css_file.read()
-                file_length = len(bytearray)
-                self.request.sendall((f"HTTP/1.1 200 OK\r\nContent-Length:"+str(file_length)+"\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/css; charset=utf-8\r\n\r\n").encode()+bytearray)
-        
-        elif b'/script.js' in request[0]:
-            with open("frontend/script.js", "rb") as css_file:
-                bytearray = css_file.read()
-                file_length = len(bytearray)
-                self.request.sendall((f"HTTP/1.1 200 OK\r\nContent-Length:"+str(file_length)+"\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/css; charset=utf-8\r\n\r\n").encode()+bytearray)
-        
-        elif b'/items.js' in request[0]:
-            with open("frontend/items.js", "rb") as css_file:
-                bytearray = css_file.read()
-                file_length = len(bytearray)
-                self.request.sendall((f"HTTP/1.1 200 OK\r\nContent-Length:"+str(file_length)+"\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/css; charset=utf-8\r\n\r\n").encode()+bytearray)
-        
-        elif b'/styleguide.js' in request[0]:
-            with open("frontend/styleguide.js", "rb") as css_file:
-                bytearray = css_file.read()
-                file_length = len(bytearray)
-                self.request.sendall((f"HTTP/1.1 200 OK\r\nContent-Length:"+str(file_length)+"\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/css; charset=utf-8\r\n\r\n").encode()+bytearray)                                
-        
-        elif b'/style.css' in request[0]:
-            with open("frontend/style.css", "rb") as css_file:
-                bytearray = css_file.read()
-                file_length = len(bytearray)
-                self.request.sendall((f"HTTP/1.1 200 OK\r\nContent-Length:"+str(file_length)+"\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/css; charset=utf-8\r\n\r\n").encode()+bytearray)
-        
-        else:
-           self.request.sendall("HTTP/1.1 404 Not Found\r\nContent-Length: 5\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nError".encode())
-    
+def js():
+   return render_template('frontend/main.js')
+
+if __name__ == '__main__':
+    app.run()
 
 
-if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = 6000
-
-    server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler)
-    server.serve_forever()
