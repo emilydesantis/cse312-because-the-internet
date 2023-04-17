c5eab32d951a7ccc7deb6f49efff7f7ea4ebac61
alkesnaf <alkesnaf@buffalo.edu>
2023-04-17 12:29:35 -0400

    git reset HEAD^Merge branch 'main' of github.com:emilydesantis/cse312-because-the-internet

---
+++ server.py
@@ -0,0 +1,24 @@
+import socketserver
+import database
+import json
+import hashlib
+import base64
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def html():
+   return render_template('frontend/index.html')
+
+def css():
+   return render_template('frontend/style.css')
+
+def js():
+   return render_template('frontend/main.js')
+
+if __name__ == '__main__':
+    app.run()
+
+
