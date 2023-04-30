8a3998f15fd995f40921671019dbbbbc47f09b31
emilydesantis <emilydes2@gmail.com>
2023-04-30 19:09:45 -0400

    updated and cleaned up files

--- frontend/loading.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function loadContainer(container) {
-    // https://www.html5gamedevs.com/topic/30539-correct-way-to-remove-sprites-container/
-    app.stage.removeChildren();
-    app.stage.addChild(container);
-}
